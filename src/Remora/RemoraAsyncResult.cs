6daf1f803db4ceb78fd1e6a5532d38d60dbb2890
Julien Blin <julien.blin@gmail.com>
2011-10-16 11:57:23 -0400

    Added basic Remora async handler.

---
+++ src/Remora/RemoraAsyncResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+
+namespace Remora
+{
+    public class RemoraAsyncResult : IAsyncResult
+    {
+        private readonly AsyncCallback _callback;
+
+        public RemoraAsyncResult(AsyncCallback cb, HttpContext context, Object state)
+        {
+            _callback = cb;
+            Context = context;
+            AsyncState = state;
+        }
+
+        public bool IsCompleted { get; private set; }
+        public WaitHandle AsyncWaitHandle { get { return null; } }
+        public object AsyncState { get; private set; }
+        public bool CompletedSynchronously { get { return false; } }
+        public HttpContext Context { get; private set; }
+
+        public void Process()
+        {
+
+        }
+    }
+}
---
+++ src/Remora/RemoraHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Remora
+{
+    public class RemoraHandler : IHttpAsyncHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new InvalidOperationException("This http handler is asynchronous.");
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public IAsyncResult BeginProcessRequest(HttpContext context, AsyncCallback cb, object extraData)
+        {
+            var result = new RemoraAsyncResult(cb, context, extraData);
+            result.Process();
+            return result;
+        }
+
+        public void EndProcessRequest(IAsyncResult result)
+        {
+            
+        }
+    }
+}
