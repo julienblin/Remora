0947aa5bfc4fd016b4e71da06e32b29e74cbd50d
Julien Blin <julien.blin@gmail.com>
2011-10-19 11:33:27 -0400

    Added SoapTransformer.LoadSoapDocument.

--- src/Remora.Tests/Transformers/Impl/SoapTransformerTest.cs
+++ src/Remora.Tests/Transformers/Impl/SoapTransformerTest.cs
@@ -3,12 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Remora.Core.Impl;
+using Remora.Extensions;
+using Remora.Transformers.Impl;
 
 namespace Remora.Tests.Transformers.Impl
 {
     [TestFixture]
     public class SoapTransformerTest : BaseTest
     {
+        private SoapTransformer _transformer;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _transformer = new SoapTransformer { Logger = GetConsoleLogger() };
+        }
+
+        [Test]
+        public void It_should_validate_arguments()
+        {
+            Assert.That(() => _transformer.LoadSoapDocument(null),
+                Throws.Exception.TypeOf<ArgumentNullException>()
+                .With.Message.Contains("message"));
+        }
+
+        [Test]
+        public void It_should_load_a_soap_document()
+        {
+            var message = new RemoraRequest
+            {
+                ContentEncoding = Encoding.UTF8,
+                Data = LoadSample("SimpleHelloWorldRequest.xml").ReadFully(0)
+            };
+
+            var result = _transformer.LoadSoapDocument(message);
+            Assert.That(result.Descendants(SoapTransformer.SoapEnvelopeNamespaceLinq + "Body").Count(), Is.EqualTo(1));
+        }
     }
 }
--- src/Remora/Transformers/Impl/SoapTransformer.cs
+++ src/Remora/Transformers/Impl/SoapTransformer.cs
@@ -4,18 +4,44 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using Castle.Core.Logging;
 using Remora.Core;
+using Remora.Exceptions;
+using Remora.Extensions;
 
 namespace Remora.Transformers.Impl
 {
     public class SoapTransformer : ISoapTransformer
     {
+        public const string SoapEnvelopeNamespace = @"http://schemas.xmlsoap.org/soap/envelope/";
+        public const string SoapEnvelopeNamespaceLinq = "{" + SoapEnvelopeNamespace + "}";
+
+        private ILogger _logger = NullLogger.Instance;
+        /// <summary>
+        /// Logger
+        /// </summary>
+        public ILogger Logger
+        {
+            get { return _logger; }
+            set { _logger = value; }
+        }
+
         public XDocument LoadSoapDocument(IRemoraMessage message)
         {
             if(message == null) throw new ArgumentNullException("message");
             Contract.EndContractBlock();
 
-            throw new NotImplementedException();
+            try
+            {
+                if(Logger.IsDebugEnabled)
+                    Logger.DebugFormat("Loading soap document from {0} with content encoding {1}...", message.Uri, message.ContentEncoding.EncodingName);
+
+                return XDocument.Parse(message.GetDataAsString());
+            }
+            catch (Exception ex)
+            {
+                throw new SoapTransformerException(string.Format("Unable to create a XDocument from message {0}.", message), ex);
+            }
         }
     }
 }
