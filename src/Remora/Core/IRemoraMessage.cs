678b7f3056e4f91db1a104914badd842cdd77000
Julien Blin <julien.blin@gmail.com>
2011-10-19 09:53:21 -0400

    Introduced IRemoraMessage.

---
+++ src/Remora/Core/IRemoraMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Remora.Core
+{
+    public interface IRemoraMessage
+    {
+        Encoding ContentEncoding { get; set; }
+
+        byte[] Data { get; set; }
+    }
+}
--- src/Remora/Core/IRemoraRequest.cs
+++ src/Remora/Core/IRemoraRequest.cs
@@ -30,14 +30,12 @@ namespace Remora.Core
     /// <summary>
     /// Abstract request
     /// </summary>
-    public interface IRemoraRequest
+    public interface IRemoraRequest : IRemoraMessage
     {
         Uri Uri { get; set; }
 
         IDictionary<string, string> HttpHeaders { get; }
 
         string Method { get; set; }
-
-        byte[] Data { get; set; }
     }
 }
