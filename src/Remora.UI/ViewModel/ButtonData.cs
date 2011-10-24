c942ff8de885ca484e59cbc802d8bff671139ccf
Julien Blin <julien.blin@gmail.com>
2011-10-24 08:33:07 -0400

    save work.

---
+++ src/Remora.UI/ViewModel/ButtonData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Remora.UI.ViewModel
+{
+    public class ButtonData : ControlData
+    {
+    }
+}
---
+++ src/Remora.UI/ViewModel/TraceViewer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace Remora.UI.ViewModel
+{
+    public static class TraceViewer
+    {
+        private static object _lockObject = new object();
+        private static Dictionary<string, ControlData> _dataCollection = new Dictionary<string, ControlData>();
+
+        public static ControlData Open
+        {
+            get
+            {
+                lock (_lockObject)
+                {
+                    var Str = "Open";
+
+                    if (!_dataCollection.ContainsKey(Str))
+                    {
+                        var buttonData = new ButtonData()
+                        {
+                            SmallImage = new Uri("/Remora.UI;component/Images/Font_16x16.png", UriKind.Relative),
+                            ToolTipTitle = "Open (Ctrl+O)",
+                            ToolTipDescription = "Open a directory containing trace files."
+                        };
+                        _dataCollection[Str] = buttonData;
+                    }
+
+                    return _dataCollection[Str];
+                }
+            }
+        }
+    }
+}
