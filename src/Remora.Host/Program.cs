360d1c88c53376a29c75e52039ffd236e71db05b
Julien Blin <julien.blin@gmail.com>
2011-11-02 10:12:33 -0400

    Added log4net configuration in Program.

--- src/Remora.Host/Program.cs
+++ src/Remora.Host/Program.cs
@@ -1,29 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
+using log4net;
+using log4net.Core;
+using Remora.Host.Configuration;
 using Topshelf;
 
 namespace Remora.Host
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            HostFactory.Run(x =>
-            {
-                //x.Service<TownCrier>(s => 
-                //{
-                //    s.ConstructUsing(name => new TownCrier());
-                //    s.WhenStarted(tc => tc.Start());
-                //    s.WhenStopped(tc => tc.Stop());
-                //});
-                //x.RunAsLocalSystem();
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
 
-                //x.SetDescription("Sample Topshelf Host");
-                //x.SetDisplayName("Stuff");
-                //x.SetServiceName("stuff");
-            });
+        static int Main(string[] args)
+        {
+            ConfigureLog4Net();
+
+            try
+            {
+                var config = RemoraHostConfigurationSectionHandler.GetConfiguration();
+
+                HostFactory.Run(x =>
+                {
+                    //x.Service<TownCrier>(s => 
+                    //{
+                    //    s.ConstructUsing(name => new TownCrier());
+                    //    s.WhenStarted(tc => tc.Start());
+                    //    s.WhenStopped(tc => tc.Stop());
+                    //});
+                    //x.RunAsLocalSystem();
+
+                    x.SetDescription(config.Description);
+                    x.SetDisplayName(config.DisplayName);
+                    x.SetServiceName(config.ServiceName);
+                });
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal("There has been an error while starting Remora host.", ex);
+                return -1;
+            }
+        }
+
+        private static void ConfigureLog4Net()
+        {
+            if (ConfigurationManager.GetSection("log4net") == null)
+            {
+                var layout = new log4net.Layout.SimpleLayout();
+                layout.ActivateOptions();
+                var consoleAppender = new log4net.Appender.ColoredConsoleAppender
+                {
+                    Layout = layout
+                };
+                consoleAppender.AddFilter(new log4net.Filter.LevelRangeFilter { LevelMin = Level.Info, AcceptOnMatch = true });
+                consoleAppender.ActivateOptions();
+                log4net.Config.BasicConfigurator.Configure(consoleAppender);
+            }
+            else
+            {
+                log4net.Config.XmlConfigurator.Configure();
+            }
         }
     }
 }
