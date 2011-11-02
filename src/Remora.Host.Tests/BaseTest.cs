ae2bee22a8a1aa508bd728c60496a19191a0ae06
Julien Blin <julien.blin@gmail.com>
2011-11-02 08:25:27 -0400

    Added Remora.Host and Remora.Host.Tests projects.

---
+++ src/Remora.Host.Tests/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Core.Logging;
+using Castle.Facilities.Logging;
+using Castle.Windsor;
+
+namespace Remora.Host.Tests
+{
+    public abstract class BaseTest
+    {
+        private IWindsorContainer _container;
+
+        protected ILogger GetConsoleLogger()
+        {
+            if (_container == null)
+            {
+                _container = new WindsorContainer();
+                _container.AddFacility<LoggingFacility>(f => f.LogUsing(LoggerImplementation.Console));
+            }
+            return _container.Resolve<ILogger>();
+        }
+    }
+}
---
+++ src/Remora.Host/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topshelf;
+
+namespace Remora.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                //x.Service<TownCrier>(s => 
+                //{
+                //    s.SetServiceName("tc");
+                //    s.ConstructUsing(name => new TownCrier());
+                //    s.WhenStarted(tc => tc.Start());
+                //    s.WhenStopped(tc => tc.Stop());
+                //});
+                //x.RunAsLocalSystem();
+
+                //x.SetDescription("Sample Topshelf Host");
+                //x.SetDisplayName("Stuff");
+                //x.SetServiceName("stuff");
+            });
+        }
+    }
+}
